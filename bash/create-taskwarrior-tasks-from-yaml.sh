708621176e1c08b22c148e6c238f0ac994957dc3
Markus Malz <markus.malz@outlook.com>
2025-08-13 13:35:09 +0200

    auto

--- bash/create-taskwarrior-tasks-from-yaml.sh
+++ bash/create-taskwarrior-tasks-from-yaml.sh
@@ -1,54 +1,62 @@
-#!/usr/bin/env bash
 
 
-# Requires: yq (https://github.com/mikefarah/yq), taskwarrior
+# Requires: taskwarrior
 
-# Accept YAML file path as first argument, default to 'tasks.yaml' if not provided
-YAML_FILE="${1:-tasks.yaml}"
-
-declare -A TASK_IDS
 
 add_task() {
     local name="$1"
-    local parent_id="$2"
+    local child_ids="$2"
     local id
-
-    # Add the task, get its ID
-    if [ -n "$parent_id" ]; then
-        id=$(task add "$name" depends:$parent_id | grep -oE 'Created task ([0-9]+)' | awk '{print $3}')
+    if [ -n "$child_ids" ]; then
+        id=$(task add "$name" depends:$child_ids | grep -oE 'Created task ([0-9]+)' | awk '{print $3}')
     else
         id=$(task add "$name" | grep -oE 'Created task ([0-9]+)' | awk '{print $3}')
     fi
     echo "$id"
 }
 
-parse_yaml() {
-    local parent_id="$1"
-    local prefix="$2"
-    local keys
-    keys=$(yq e "$prefix | keys" "$YAML_FILE" | sed 's/- //g')
 
-    for key in $keys; do
-        local task_name
-        task_name=$(yq e "$prefix.$key.task" "$YAML_FILE")
-        if [ "$task_name" == "null" ]; then
-            task_name="$key"
-        fi
+create_taskwarrior_tasks_from_yaml() {
+    local YAML_FILE="${1:-tasks.yaml}"
+    local -a task_names
+    local -a indent_levels
+    local -a task_ids
+    local -a child_map
+    local line indent task_name i
 
-        local id
-        id=$(add_task "$task_name" "$parent_id")
-        TASK_IDS["$prefix.$key"]=$id
+    # Read all lines and store their indent and name
+    while IFS= read -r line || [ -n "$line" ]; do
+        [[ -z "$line" ]] && continue
+        indent=$(echo "$line" | sed -E 's/^([ ]*).*/\1/' | wc -c)
+        indent=$((indent - 1))
+        task_name=$(echo "$line" | sed -E 's/^\s*task:\s*//;s/^\s*//;s/\s*$//')
+        task_names+=("$task_name")
+        indent_levels+=("$indent")
+    done < "$YAML_FILE"
 
-        # Check for subtasks
-        local subkeys
-        subkeys=$(yq e "$prefix.$key | keys" "$YAML_FILE" | sed 's/- //g')
-        if [ -n "$subkeys" ] && [ "$subkeys" != "task" ]; then
-            parse_yaml "$id" "$prefix.$key"
-        fi
+    # Build child map: for each task, collect only immediate children (next deeper indentation)
+    for ((i=0; i<${#task_names[@]}; i++)); do
+        child_map[$i]=""
+        local this_indent=${indent_levels[$i]}
+        local children=""
+        for ((j=i+1; j<${#task_names[@]}; j++)); do
+            if [ ${indent_levels[$j]} -eq $((this_indent + 2)) ]; then
+                children+="$j "
+            elif [ ${indent_levels[$j]} -le $this_indent ]; then
+                break
+            fi
+        done
+        child_map[$i]="$children"
     done
+
+    # Create tasks in reverse order so children are created before parents
+    for ((i=${#task_names[@]}-1; i>=0; i--)); do
+        local child_ids=""
+        for child_idx in ${child_map[$i]}; do
+            child_ids+="${task_ids[$child_idx]},"
+        done
+        child_ids=${child_ids%,} # Remove trailing comma
+        task_ids[$i]=$(add_task "${task_names[$i]}" "$child_ids")
+    done
+    echo "Tasks added with dependencies."
 }
-
-# Start parsing from the root
-parse_yaml "" "."
-
-echo "Tasks added with dependencies."
