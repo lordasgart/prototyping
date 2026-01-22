f5b71e004994cb6c97d3592de614609a5cd8a9f3
Markus Malz <markus.malz@outlook.com>
2026-01-22 20:38:44 +0100

    auto

--- cs/Noventis/ActionDispatcher/src/Noventis.ActionDispatcher.Web/Program.cs
+++ cs/Noventis/ActionDispatcher/src/Noventis.ActionDispatcher.Web/Program.cs
@@ -2,6 +2,13 @@ using Noventis.ActionDispatcher.Web.Components;
 
 var builder = WebApplication.CreateBuilder(args);
 
+// Configure Kestrel to bind to both localhost and network interface
+builder.WebHost.ConfigureKestrel(options =>
+{
+    options.ListenAnyIP(5026);
+    options.ListenLocalhost(5027);
+});
+
 // Add services to the container.
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
@@ -13,7 +20,7 @@ if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Error", createScopeForErrors: true);
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
+    //app.UseHsts();
 }
 
 app.UseHttpsRedirection();
